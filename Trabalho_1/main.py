2527f5cecfd81c9652509eee748fb0df96f134b2
Arcoverde <plvaa03@gmail.com>
2022-07-31 02:00:59 -0300

    Update main.py

--- Trabalho_1/main.py
+++ Trabalho_1/main.py
@@ -102,8 +102,8 @@ def ataque (msg_cifrada: str, idioma):
 
 # encontrar o comprimento provavel da chave
     espacamento = []
-    max_chave = 50
-    tolerancia = 10
+    max_chave = 100
+    tolerancia = 50
 
     for i in range(len(msg_cifrada) - 2):
         tmp = msg_cifrada[i] + msg_cifrada[i+1] + msg_cifrada[i+2]
