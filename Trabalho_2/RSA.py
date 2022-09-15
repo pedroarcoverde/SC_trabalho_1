16cb3beaa3fc590c38c77524a81ef2562edf20de
Marcelo Piano <marcelopianopatuscosantiago@gmail.com>
2022-09-15 11:58:11 -0300

    feat: geraD

--- Trabalho_2/RSA.py
+++ Trabalho_2/RSA.py
@@ -1,97 +1,9 @@
 import random
 import math
-from random import randint
-
-
-# Verifica se d ≡ e−1 (mod λ(n)) que é se o resto da divisão de d por λ(n) for e**(-1)
-def isModularMultiplicativeInversive(d,e,oDn):
-    return print('fazer')
-
-# Verifica se 2 números são coprimos
-def isCoprime(x, y):
-    if math.gcd(x, y) == 1:
-        return True
-    else:
-        return False
-
-# gera um número 'aleatório' de tamanho determinado em Bits
-
-
-def geraNum(tamBits):
-    num = random.randrange(2**(tamBits - 1) + 1, 2**tamBits - 1)
-    return num
-
-
-# verifica se é primo pelo método sieve Eratosthenes
-listaPrimos = []
-
-
-def sieveEratosthenes(n):
-    primo = [True for i in range(n + 1)]
-    p = 2
-    while (p*p <= n):
-        if (primo[p] == True):
-            for i in range(p*p, n+1, p):
-                primo[i] = False
-        p += 1
-
-    # Loop passando da lista de O(n) para uma lista APENAS com numeros primos
-    for p in range(2, n+1):
-        if primo[p]:
-            listaPrimos.append(p)
-
-# verifica se o numero gerado é primo e retorna
-
-
-def geraPrimo(tamBits):
-    while True:
-        primo = geraNum(tamBits)
-
-        # Divisao pelos primeiros numeros primos calculados pelo sieveEratosThenes
-        for divisor in listaPrimos:
-            if primo % divisor == 0 and divisor**2 <= primo:
-                break
-        else:
-            return primo
 
 ########### implementar confirmação probalistica deq primo realmente é primo ##########################
-
-
-def geradorPeQ():
-    tamBits = 1024  # tamanho de P e Q em Bits
-    pouq = gera_primo2(tamBits)
-    return pouq
-
-
-def geraE(oDn):
-
-    while True:
-        num = random.randrange(2, oDn - 1)
-        if isCoprime(num, oDn):
-            break
-
-    return num
-
-
-def geraD(e, oDn):
-    
-    while True:
-        num = random.randrange(1, oDn - 1)
-        if isModularMultiplicativeInversive(num, e, oDn):
-            break
-    
-    return num
-
-
-def cifracao(chave, textoplano):
-    return
-
-
-def decifracao(chave, cifra):
-    return
-
-
-def gera_primo2(tamanho):
+# Nova implementação para gerar o primo
+def gera_primo(tamanho):
 
     while True:
         x = random.randrange(1 << (tamanho-1), (1 << tamanho) - 1)
@@ -105,7 +17,9 @@ def ehPrimo(n):
     while m % 2 == 0:
         k += 1
         m>>=1
-    assert((2**k) * m == n - 1)
+       
+    if (2**k) * m != n - 1:
+        return False
 
     for i in range(8):
         a = random.randrange(2, n)
@@ -118,3 +32,39 @@ def ehPrimo(n):
                 return False
 
     return True
+
+
+def geradorPeQ():
+    tamBits = 1024  # tamanho de P e Q em Bits
+    pouq = gera_primo(tamBits)
+    return pouq
+
+
+def geraE(oDn):
+
+    while True:
+        num = random.randrange(2, oDn)
+        if math.gcd(oDn, num) == 1:
+            break
+
+    return num
+
+def geraD(e, oDn):
+    return modularInversion(e, oDn)[1] % oDn
+    
+
+def modularInversion(e, oDn):
+    if e == 0:
+        return (oDn, 0, 1)
+    else:
+        a, b, c = modularInversion(oDn % e, e)
+        return (a, c - (oDn // e) * b, b)
+
+
+def cifracao(chave, textoplano):
+    return
+
+
+def decifracao(chave, cifra):
+    return
+
--- Trabalho_2/main.py
+++ Trabalho_2/main.py
@@ -1,75 +1,41 @@
-import sympy
-import math
+from RSA import *
+import base64, hashlib
+import sys
+sys.setrecursionlimit(1500)
+### PARTE I
 
-def carmichael(n):
-    coprimos = [x for x in range(1, n) if math.gcd(x, n) == 1]
-    k = 1
-    while not all(pow(x, k, n) == 1 for x in coprimos):         #pow(A, B, C) efficiently computes pow(A, B) % C.
-        k += 1
-    return k
+## Gera duas tuplas com as chaves públicas e privadas
 
-def isCoprime(x,y):
-    if math.gcd(x,y) == 1:
-        return True
-    else:
-        return False
-
-def isModularMultiplicativeInversive(d,e,l):
-    if (e * d) % l == 1:
-        return True
-    else:
-        return False
-
-
-# Parte I: Geração de chaves
-# Geração de chaves (p e q primos com no mínimo de 1024 bits)
-
-'''testar com os valores
-    p = 61
-    q = 53
-    e = 17
-    d = 413
-'''
-
-tamanhoMaximo = 10
-
+# Gera p e q distintos
+p = geradorPeQ() 
 while True:
-    p = int(input(f"Escolha um número primo P maior que {tamanhoMaximo}:\n"))
-    if p < tamanhoMaximo:
-        print(f"P deve ser maior que {tamanhoMaximo}")
-    elif not sympy.isprime(int(p)):
-        print('q não é primo')
-    else:
+    q = geradorPeQ()
+    if (p != q):
         break
 
-while True:
-    q = int(input(f"Escolha um número primo Q maior que {tamanhoMaximo}:\n"))
-    if p < tamanhoMaximo:
-        print(f"P deve ser maior que {tamanhoMaximo}")
-    elif not sympy.isprime(int(q)):
-        print("q não é primo!")
-    else:
-        break
+print(f"P = {p}\n\n")
+print(f"Q = {q}\n\n")
 
-n = p*q
+# 'n' e 'O(n)'
+n = p * q
+oDn = (p - 1) * (q - 1)
 
-lam = carmichael(n)
+# Chaves públicas E e D
+e = geraE(oDn)
 
-while True:
-    e = int(input(f"Escolha um inteiro E que pertence ao intervalo 1 < e < {lam} e é coprimo de {lam}:\n"))
-    if e < 1 and e > lam:
-        print('Não pertence ao intervalo.')
-    elif not isCoprime(e,lam):
-        print(f'Não é coprimo de {lam}')
-    else:
-        break
+d = geraD(e, oDn)
 
-while True:
-    d = int(input(f"Escolha um inteiro D em que D x {e} = 1 mod({lam}):\n"))
-    if not isModularMultiplicativeInversive(d,e,lam):
-        print("Não é Modular Multiplicative Inversive.")
-    else:
-        break
+print(f"Suas chaves públicas são\n n = {n}\n e = {e}\n")
+print(f"Suas chaves privadas são\n n = {n}\n d = {d}\n")
 
-print(f"Suas chaves públicas são n = {n}, e = {e}")
-print(f"Suas chaves privadas são n = {n}, d = {d}")
+# Chaves em formato de tuplas
+chavePublica = (e, n)
+chavePrivada = (d, n)
+
+
+
+### PARTE II
+
+### PARTE III
+
+### PARTE IV
